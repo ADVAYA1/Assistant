facd50f8361d3530eb434f4e9479b16d07415dc8
ADVAYA1 <158846147+ADVAYA1@users.noreply.github.com>
2025-09-11 01:13:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ import geminiResponse  from "./gemini.js";
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://assistant-frontend-ecru.vercel.app",
     credentials: true, // Allow credentials to be sent
 }));
 
